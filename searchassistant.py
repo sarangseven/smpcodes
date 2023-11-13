c068a67cbe1fcd42952d6c9e7d97a5bbe0fd9f26
SARANG S <97600370+sarangseven@users.noreply.github.com>
2023-11-13 21:48:32 +0530

    Delete searchassistant.py

--- searchassistant.py
+++
@@ -1,9 +0,0 @@
-import pyttsx3
-import wikipedia
-
-voice=pyttsx3.init()
-In=input("searching wikipedia/google:")
-result=wikipedia.summary(In,sentences=3)
-print(result)
-voice.say(result)
-voice.runAndWait()
